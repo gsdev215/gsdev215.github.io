a9167d7b053001f9199cbe643e492d6b1c6042c5
Dev Bhattacharjee <74264993+gsdev215@users.noreply.github.com>
2025-11-13 14:03:53 +0530

    Create std.py

---
+++ f/std.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Boilerplate Code
+Author: Dev Bhattacharjee (gsdev)
+Description: Standard Python file template.
+"""
+
+# --- Std Libs --- #
+import sys
+import io
+from typing import List, Dict, Union
+
+# --- Installed Packages --- #
+
+# --- Constants --- #
+
+# --- Variables --- #
+
+
+def main(args: List[str]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
